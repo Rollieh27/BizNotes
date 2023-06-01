e274877d36e5cc03b37db24157b9f35574b3e806
Rollieh27 <119282670+Rollieh27@users.noreply.github.com>
2023-06-01 03:36:15 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const notesRouter = require("./routes/notesRoutes.js");
-const htmlRouter = require("./routes/htmlRoutes.js");
+const notesRouter = require("./routes/notesRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
